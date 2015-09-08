8ae6b394d3bdf8557513a06cc5e3f16f4e6e390e
rpaskin <ronnie@megaputz.com>
2015-09-08 15:46:40 -0300

    Adiciona seed_dumper e prepara alguns seeds de categorias; atenção: sugiro usar nomes em inglês

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create!([
+  {nome: "PR"},
+  {nome: "EL"},
+  {nome: "BT"},
+  {nome: "CS"},
+  {nome: "Médio"},
+  {nome: "Fundamental"},
+  {nome: "Professores"},
+  {nome: "Funcionários"}
+])
+
+# Post.create!([
+#   {titulo: "testando", conteudo: "dfmvdk kfdmv dfklvmldm fklv mldf vkldfmklvm klfm lkd", category_id: 1},
+#   {titulo: "Circuitos impressos", conteudo: "Ter um forno microondas e uma impressora laser para fazer circuitos impressos no ORT", category_id: 2}
+# ])
