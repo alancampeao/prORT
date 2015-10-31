44f298c664aba475125cd801fdf7a49076983318
vitoraraujo <vitoraraujow@gmail.com>
2015-10-31 01:19:09 +0000

    Adiciona o modo admin ao usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021200757) do
+ActiveRecord::Schema.define(version: 20151029235314) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151021200757) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
