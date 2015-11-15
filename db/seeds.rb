03ff144615f895b3384ce96b98fbdbc0ff73e74c
Thor Garcia <thor.fc21@gmail.com>
2015-11-15 10:26:35 -0200

    Algumas alterações de design.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ Category.create!([
 ])
 
 # Post.create!([
-#   {titulo: "testando", conteudo: "dfmvdk kfdmv dfklvmldm fklv mldf vkldfmklvm klfm lkd", category_id: 1},
-#   {titulo: "Circuitos impressos", conteudo: "Ter um forno microondas e uma impressora laser para fazer circuitos impressos no ORT", category_id: 2}
+#   {titulo: "testando", conteudo: "dfmvdk kfdmv dfklvmldm fklv mldf vkldfmklvm klfm lkd", category_id: 1, user_id: 1},
+#   {titulo: "Circuitos impressos", conteudo: "Ter um forno microondas e uma impressora laser para fazer circuitos impressos no ORT", category_id: 2, user_id: 2}
 # ])
